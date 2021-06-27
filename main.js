a6541e7daa3b748a162344a4ead136a72510634b
ryry <ryanpetrecca@gmail.com>
2021-06-26 22:17:12 -0700

    integrated a zen quotes function to the bot

--- main.js
+++ main.js
@@ -13,6 +13,26 @@ client.on("message", msg => {
   }
 })
 
+const fetch = require("node-fetch")
+
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+      return res.json()
+      })
+    .then(data => {
+      return data[0]["q"] + " -" + data[0]["a"]
+    })
+}
+
+client.on("message", msg => {
+  if (msg.author.bot) return
+    
+  if (msg.content === "$inspire") {
+    getQuote().then(quote => msg.channel.send(quote))
+  }
+})
+
 client.on('message', message => {
   // If the message is "what is my avatar"
   if (message.content === 'what is my avatar') {
